477de18037d9884cd279c969ab2aa04fb46f87e3
vimal-vijayan <vvimal44@gmail.com>
2025-01-23 22:02:05 +0100

    updated homepage

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -5,61 +5,51 @@ import {
   Page,
   ItemCardGrid,
 } from '@backstage/core-components';
-
 import {
   HomepageCompositionRoot,
   HomePageCompanyLogo,
   HomePageToolkit,
   HomePageStarredEntities,
 } from '@backstage/plugin-home';
+import { HomePageSearchBar } from '@backstage/plugin-search';
 
-import { HomePageSearchBar } from '@backstage/plugin-search'; 
-// If you haven't set up search, remove this import and the <HomePageSearchBar /> below.
+// Option 1: Import the logo (if using webpack module imports)
+import logo from '../Root/logo/Essity.png';
 
 export const HomePage = () => {
   return (
     <Page themeId="home">
-      {/* A simple Header; feel free to remove or replace */}
-      <Header title="Welcome to Backstage" subtitle="Home" />
-
+      <Header 
+      title="Welcome to Backstage" 
+      />
       <Content>
-        {/* The Homepage plugin's layout container */}
-        <HomepageCompositionRoot>
-          {/* Example: Company logo at the top */}
-          <HomePageCompanyLogo
-            logo="My Company"
-            // Provide a logoUrl if you have one. You could also leave it blank to show text only.
-            // logoUrl="/path/to/my-company-logo.png"
-          />
+      <HomepageCompositionRoot>
+        {/* Option 1: Using imported logo */}
+        <HomePageCompanyLogo
+        logo={<img src={logo} alt="Essity Logo" style={{ width: '150px' }} />}
+        />
+        <HomePageSearchBar />
+        
+        <HomePageToolkit
+        title="Quick Links"
+        tools={[
+          {
+          url: 'https://backstage.io/docs',
+          label: 'Backstage Docs',
+          icon: <></>,
+          },
+          {
+          url: 'https://my-internal-wiki.company.com',
+          label: 'Internal Wiki',
+          icon: <></>,
+          },
+        ]}
+        />
 
-          {/* A search bar, if you've configured the Backstage search plugin */}
-          <HomePageSearchBar />
-
-          {/* A "Toolkit" area to showcase various quick links or calls to action */}
-          <HomePageToolkit
-            title="Quick Links"
-            // Example items
-            tools={[
-              {
-                url: 'https://backstage.io/docs',
-                label: 'Backstage Docs',
-                icon: <></>, // Insert a Material UI or custom icon if desired
-              },
-              {
-                url: 'https://my-internal-wiki.company.com',
-                label: 'Internal Wiki',
-                icon: <></>,
-              },
-            ]}
-          />
-
-          {/* Starred entities for the signed-in user */}
-          <ItemCardGrid>
-            <HomePageStarredEntities />
-          </ItemCardGrid>
-
-          {/* Add more homepage sections here as needed */}
-        </HomepageCompositionRoot>
+        {/* <ItemCardGrid>
+        <HomePageStarredEntities />
+        </ItemCardGrid> */}
+      </HomepageCompositionRoot>
       </Content>
     </Page>
   );
